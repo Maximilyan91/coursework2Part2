22f6adf6557c8e94004c9bb101c2367e9064dc24
Максим <gmax7911@gmail.com>
2023-02-17 14:48:26 +0300

    разработка

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        List<String> words = new ArrayList<>();
+        String word = "yourapp the quick brown fox jumps over the lazy dog";
+        String[] split = word.split(" ");
+        for (String w : split) {
+            words.add(w);
+        }
+        System.out.println(words);
     }
 }
